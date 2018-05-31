41d31ab3d16107dafb06f3edd708eee05fc5d9f6
AC Violet <30395886+m13953842591@users.noreply.github.com>
2018-05-31 21:49:48 +0800

    sort update

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "sort.h"
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    int arr[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    bubbleSort(arr, 10);
+    for(int i= 0; i < 10; i++)
+    {
+        cout << arr[i] << ' ';
+    }
     return 0;
 }
--- Sort/sort.h
+++ Sort/sort.h
@@ -1,5 +1,8 @@
 #ifndef SORT_H_INCLUDED
 #define SORT_H_INCLUDED
+#include <iostream>
+using namespace std;
+
 template <class T>
 void swap(T arr[], int a, int b)
 {
@@ -8,12 +11,136 @@ void swap(T arr[], int a, int b)
     arr[b] = tmp;
 }
 template <class T>
-void quickSort(T arr[], int left, int right)
+int divide(T arr[], int low, int high)
 {
-    int tmp = (left + right) / 2;
-    if(arr[left] < arr[tmp] && arr[left] < arr[right])
-        swap(arr, left, tmp);
-    
+    if (low >= high)
+        return low;
+    T key = arr[low];
+    while (low < high)
+    {
+        while (low < high && arr[high] >= key)
+            high--;
+        if (low < high)
+            arr[low] = arr[high];
+        while (low < high && arr[low] <= key)
+            low++;
+        if (low < high)
+            arr[high] = arr[low];
+    }
+    arr[low] = key;
+    return low;
+}
+template <class T>
+void quickSort(T arr[], int low, int high)
+{
+    int mid;
+    if (low >= high)
+        return;
+    mid = divide(arr, low, high);
+    quickSort(arr, low, mid - 1);
+    quickSort(arr, mid + 1, high);
+}
+
+template <class T>
+void insertSort(T arr[], int size)
+{
+    int i, j;
+    int key;
+    for (i = 1; i < size; i++)
+    {
+        key = arr[i];
+        for (j = i - 1; j >= 0 && arr[j] > key; j--)
+            arr[j + 1] = arr[j];
+        arr[j + 1] = key;
+    }
+}
+
+template <class T>
+void shellSort(T arr[], int size)
+{
+    int key, i, j;
+    for (int step = size / 2; step > 0; step /= 2)
+    {
+        for (i = step; i < size; i++)
+        {
+            key = arr[i];
+            for (j = i - step; j >= 0 && arr[j] > key; j -= step)
+            {
+                arr[j + step] = arr[j];
+            }
+            arr[j + step] = key;
+        }
+    }
+}
+
+template <class T>
+void searchSort(T arr[], int size)
+{
+    int min;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
+            if (arr[j] < arr[i])
+                min = j;
+        }
+        swap(arr, i, min);
+    }
+}
+template <class T>
+void percolateDown(T arr[], int hole, int size)
+{
+    if (hole < 0 || hole >= size)
+        return;
+    T tmp = arr[hole];
+    int child;
+    while (hole * 2 + 2 < size)
+    {
+        child = hole * 2 + 1;
+        if (child != size && arr[child + 1] > arr[child])
+            child++;
+
+        if (arr[child] > tmp)
+        {
+            arr[hole] = arr[child];
+            hole = child;
+        }
+        else
+            break;
+    }
+    arr[hole] = tmp;
+}
+
+template <class T>
+void heapSort(T arr[], int size)
+{
+    int i;
+    for (i = size / 2 - 1; i >= 0; i--)
+        percolateDown(arr, i, size);
+    for (i = size - 1; i > 0; i--)
+    {
+        swap(arr, 0, i);
+        percolateDown(arr, 0, i);
+    }
+}
+
+template<class T>
+void bubbleSort(T arr[], int size)
+{
+    bool flag = true;
+    for(int i = size; flag && i > 1; i--) //size of each sort
+    {
+        flag = false;
+        for(int j = 1; j < i; j++) // j = [1, i - 1]
+        {
+            if(arr[j] < arr[j - 1])
+            {
+                //cout << "swap(" << j << ", " << j - 1 <<")" << endl;
+                swap(arr, j, j - 1);
+                flag = true;
+            }
+        }
+    }
 }
 
 
